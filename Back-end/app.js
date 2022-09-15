891631d537464efeed77ff83a74c92762141c8e1
Domino731 <d.orzechowski18@gmail.com>
2022-09-15 17:41:50 +0200

    BE - express & mongo init

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,1 +1,40 @@
-console.log("SERVER STARTED");
+require('dotenv').config({path: __dirname + '/.env'})
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
+const port = 8000;
+const dbUrl = process.env['MONGO_DB'];
+
+
+const app = express();
+
+// middleware
+// express public mode
+app.use(express.static("public"));
+// configure cors
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+// set body parser
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.use(bodyParser.json())
+// cookies
+app.use(cookieParser())
+
+// mongoDB connection
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("MONGO - success - connect to database")
+    })
+    .catch(error => console.log(error))
+
+// listening on port
+app.listen(port, () => {
+    console.log("SUCCESS: server started");
+});
