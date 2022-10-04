9f29d54d06c6ba50b8cf531106540f18dbba06f5
Domino731 <d.orzechowski18@gmail.com>
2022-10-04 17:11:58 +0200

    FE - car equipment config

---
+++ rentcarfe/src/CarConfiguration/equipment.ts
@@ -0,0 +1,61 @@
+export const CarEquipment = {
+    airConditioning: ['Automatic air conditioning',
+        'Automatic air conditioning: 2 zones',
+        'Automatic air conditioning: 3 zone',
+        'Automatic air conditioning: 4 or more zones',
+        'Manual air conditioning'
+    ],
+    roof: [
+        'Panoramic roof',
+        'Second glass sunroof - sliding and tilting el.',
+        'Glass sunroof - sliding and tilting electric',
+        'Glass sunroof - sliding and tilting manually'
+    ],
+    upholstery: [
+        'Alcantara upholstery',
+        'Part leather upholstery',
+        'Fabric upholstery',
+        'Leather upholstery',
+        'Electrically adjustable driver\'s seat',
+        'Heated driver\'s seat',
+        'Heated passenger seat',
+        'Ventilated front seats',
+        'Seat with memory setting',
+        'Rear seats ventilated',
+        'Armrests - front',
+        'Sport steering wheel',
+        'Steering wheel gear shift',
+        'Keyless entry',
+        'Keyless Go',
+        'Heated windshield',
+        'Fast charging function',
+        'Charging cable',
+    ],
+    CruiseControlType: [
+        'Cruise control',
+        'Adaptive cruise control ACC',
+        'Predictive cruise control PCC'
+    ],
+    headlightsType: [
+        'Bi-xenon lamps',
+        'Xenon lamps',
+        'Front lamps with LED technology',
+        'Laser headlights',
+        'Front distance control (when parking)',
+        'Rear distance control (when parking)',
+        'Park Assistant - parking assistant',
+        'Independent parking system',
+        '360 panoramic camera',
+        'Rear parking camera',
+        'Electric folding side mirrors',
+        'Blind spot assistant (sensor)',
+        'Lane assist',
+        'Distance control from the preceding vehicle',
+        'Autonomous steering system',
+        'Dynamic cornering lights',
+        'Lighting of the way home',
+        'Sport suspension',
+        'Air suspension',
+        'Isofix (child seat attachment points)'
+    ]
+}
